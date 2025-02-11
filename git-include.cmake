5f0abb7b3f495f63f6b67eb3e741b50f46e4335e
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2025-02-11 18:17:23 +0100

    git vesion

--- git-include.cmake
+++ git-include.cmake
@@ -7,7 +7,10 @@ if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../.git)
       OUTPUT_VARIABLE "GIT_DESCRIBE"
       ERROR_QUIET
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-    string(REGEX REPLACE "^v?([0-9]+)\\.([0-9]+)(-([0-9]+))?.*" "\\1.\\2.\\4" GIT_VERSION "${GIT_DESCRIBE}")
+    string(REGEX REPLACE "^v?([0-9]+)\\.([0-9]+)(-([0-9]+))?.*" "\\1.\\2" GIT_VERSION "${GIT_DESCRIBE}")
+    if(NOT GIT_VERSION MATCHES "^v?([0-9]+)\\.([0-9]+)-.+$")
+      string(REGEX REPLACE "^v?([0-9]+)\\.([0-9]+)-([0-9]+).*" "\\1.\\2.\\3" GIT_VERSION "${GIT_DESCRIBE}")
+    endif()
     message( STATUS "Repo version: ${GIT_VERSION}" )
   else(GIT_FOUND)
     set(GIT_VERSION unknown)
