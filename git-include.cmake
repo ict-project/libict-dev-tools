a3cc8c65b0d893065f33423f970ce96da31ff319
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2025-02-11 18:03:37 +0100

    git version

--- git-include.cmake
+++ git-include.cmake
@@ -7,8 +7,8 @@ if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../.git)
       OUTPUT_VARIABLE "GIT_DESCRIBE"
       ERROR_QUIET
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-    string(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)-([0-9]+).*" "\\1.\\2.\\3" GIT_VERSION "${GIT_DESCRIBE}")
-    message( STATUS "Git version: ${GIT_VERSION}" )
+    string(REGEX REPLACE "^v?([0-9]+)\\.([0-9]+)(-([0-9]+))?.*" "\\1.\\2.\\4" GIT_VERSION "${GIT_DESCRIBE}")
+    message( STATUS "Repo version: ${GIT_VERSION}" )
   else(GIT_FOUND)
     set(GIT_VERSION unknown)
   endif(GIT_FOUND)
