d834091d92f547753e0baf7467a802aa06208b44
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2025-02-11 17:57:56 +0100

    git version for debian

--- git-include.cmake
+++ git-include.cmake
@@ -4,9 +4,10 @@ if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../.git)
     execute_process(
       COMMAND ${GIT_EXECUTABLE} describe
       WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-      OUTPUT_VARIABLE "GIT_VERSION"
+      OUTPUT_VARIABLE "GIT_DESCRIBE"
       ERROR_QUIET
       OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string(REGEX REPLACE "^v([0-9]+)\\.([0-9]+)-([0-9]+).*" "\\1.\\2.\\3" GIT_VERSION "${GIT_DESCRIBE}")
     message( STATUS "Git version: ${GIT_VERSION}" )
   else(GIT_FOUND)
     set(GIT_VERSION unknown)
