553eb98a5b19bb60401de372cba061093cb3070d
Mariusz Ornowski <mariusz.ornowski@ict-project.pl>
2025-02-27 08:13:15 +0100

    Fix header file path in link-headers.sh to correctly create symbolic links

--- link-headers.sh
+++ link-headers.sh
@@ -13,7 +13,7 @@ for dir in $(find $TARGET_DIR -mindepth 1 -maxdepth 1 -type d -name "libict-*");
     # Przejście po wszystkich plikach nagłówkowych w katalogu
     for file in $(find $dir/source -name "*.hpp"); do
         header=$(basename $file)
-        path=$INCLUDE_DIR/$libname/$header
+        path=$INCLUDE_DIR/libict/$libname/$header
         mkdir -p $(dirname $path)
         # Jeśli plik nagłówkowy nie istnieje w katalogu include, to utwórz link symboliczny
         if [ ! -L $path ]; then
